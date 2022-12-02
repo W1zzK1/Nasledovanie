ad22d2bc75ad35be6175f7dc7603e37a016bafd4
Gorbunov Vladislav <91543904+W1zzK1@users.noreply.github.com>
2022-12-02 11:57:37 +0300

    Update Real.cpp

--- Nasledovanie/Real.cpp
+++ Nasledovanie/Real.cpp
@@ -1,1 +1,31 @@
 #include "Real.h"
+#include "Real.h"
+#pragma once
+#include "Number.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+class Real :public Number
+{
+public:
+	Real(float firstValue) :Number(firstValue) {};
+	float getLn()
+	{
+		if (firstValue < 0) {
+			cout << "Не попал";
+		}
+		else {
+			return log(firstValue);
+		}
+
+	};
+	float degree(float power)
+	{
+		return pow(firstValue, power);
+	};
+};
