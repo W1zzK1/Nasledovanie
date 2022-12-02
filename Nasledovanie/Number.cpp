d4b5a21586741128665218509fef99ad4d704742
Gorbunov Vladislav <91543904+W1zzK1@users.noreply.github.com>
2022-12-02 11:56:48 +0300

    Update Number.cpp

--- Nasledovanie/Number.cpp
+++ Nasledovanie/Number.cpp
@@ -1,1 +1,24 @@
+
 #include "Number.h"
+
+class Number
+{
+protected:
+	float firstValue;
+	float secondValue;
+
+public:
+	Number(float first, float second) {
+		firstValue = first;
+		secondValue = second;
+	}
+	Number(float first) {
+		firstValue = first;
+	}
+	float Additional() {
+		return firstValue + secondValue;
+	}
+	float Division() {
+		return firstValue / secondValue;
+	}
+};
